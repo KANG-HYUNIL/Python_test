b55949ba0bf283bcdbc9688513500e78b742a5f3
Kang-conqueror <139243766+Kang-conqueror@users.noreply.github.com>
2024-01-22 15:10:32 +0900

    가장 긴 증가하는 부분수열 11053번 백준 Dp 문제
    n^2 로직 사용해서, Dp 배열에 각 idx에, ary[idx] 가 마지막으로 큰 수일 때 부분수열의 길이를 저장, 머리로 직접 생각하지 않고 풀이와 질문 게시판을 통해 풀음, 사고력 증가 필요

---
+++ Dp-1/Baekjoon_11053.py
@@ -0,0 +1,33 @@
+import sys
+
+#가장 긴 증가하는 부분수열 11053(백준) Dp문제
+#배열의 각 idx에, 각 idx의 칸만큼 보았을 때의 최대 길이 저장하는 방식으로 접근하기\
+
+
+#입력 받기
+n = int(input())
+ary = list(map(int, sys.stdin.readline().split(sep=" ")))
+
+#가장 긴 증가하는 부분수열의 길이 저장할 배열
+dp = [1 for _ in range(n)] 
+
+
+#O(n^2) 로 풀기?
+for i in range(1, n):
+
+    for j in range(0, i):
+        
+        if ary[j] < ary[i]: #앞 쪽 숫자가 뒤 쪽 숫자보다 작다면
+            
+            dp[i] = max(dp[i], dp[j] + 1)
+
+
+print(max(dp))
+
+#예제 10 20 10 30 20 50
+
+
+
+
+
+
